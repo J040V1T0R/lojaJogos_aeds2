8bee64d29f792c67377dee11f74b89fda6a3623d
Kaiky Linhares Emilio <114748418+KaikyL7@users.noreply.github.com>
2025-08-05 19:25:39 -0300

    Atualizado arvoreVencedores.h

--- arvoreVencedores.h
+++ arvoreVencedores.h
@@ -4,10 +4,7 @@
 #include <stdio.h>
 #include "jogo.h"
 
-// Função que implementa a intercalação de partições usando uma Árvore de Vencedores
-// nome_base_particoes: prefixo dos nomes dos arquivos de partição
-// num_particoes: número de partições a serem intercaladas
-// arq_saida: arquivo final ordenado
+// função que implementa a intercalação de partições usando uma Árvore de Vencedores
 void arvore_de_vencedores(const char* nome_base_particoes, int num_particoes, FILE* arq_saida);
 
 #endif // ARVOREVENCEDORES_H_INCLUDED
