8a95df809fff59a1ce6f1615e1ec717dc951d4cf
Kaiky Linhares Emilio <114748418+KaikyL7@users.noreply.github.com>
2025-08-05 19:22:46 -0300

    Atualizada selecaoSubs.h

--- selecaoSubs.h
+++ selecaoSubs.h
@@ -4,11 +4,7 @@
 #include <stdio.h>
 #include "jogo.h"
 
-// Função que implementa a Seleção por Substituição para criar partições ordenadas
-// arq_entrada: arquivo de dados original
-// nome_base_particoes: prefixo para os nomes dos arquivos de partição (ex: "part")
-// M: tamanho do reservatório em memória
-// retorna o número de partições criadas
+// função que implementa a Seleção por Substituição para criar partições ordenadas
 int selecao_por_substituicao(FILE *arq_entrada, const char* nome_base_particoes, int M);
 
-#endif // SELECAOSUBS_H_INCLUDED
+#endif 
